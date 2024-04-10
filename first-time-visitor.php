fea3f04f3ef65863d68109b9818aa278438347e5
Ryan Miller <ryanmillergm@gmail.com>
2024-04-10 17:35:36 -0600

    Updates all pages to php to use partials

---
+++ first-time-visitor.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <!-- title -->
+        <title>First Time Visitor | Expect Hope Church</title>
+
+        <!-- description -->
+        <meta name="description" content="We can't wait to meet you. We have children's ministry for ages 0-12 and can pre-register your kids for service. We would love to meet you before service, get you connected and show you around.">
+        <!-- keywords -->
+        <meta name="keywords" content="Jesus, Christian, Salvation, Hope, Gospel, Bible, Fellowship, God">
+        
+        <?php include './includes/head.html';?>
+
+    </head>
+    <body>
+
+        <?php include './includes/header.html';?>
+
+
+
+        <!-- start page title section -->
+        <section class="p-0 one-third-screen position-relative wow animate__fadeIn">
+            <div class="opacity-medium bg-extra-dark-gray z-index-0"></div>
+            <div class="container position-relative">
+                <div class="row align-items-center">
+                    <div class="col-12 one-third-screen d-flex flex-column justify-content-center page-title-xxxl text-center">
+                        <!-- start sub title -->
+                        <span class="d-block text-white-2 opacity6 margin-10px-bottom alt-font">Expect Hope</span>
+                        <!-- end sub title -->
+                        <!-- start page title -->
+                        <h1 class="alt-font text-white-2 font-weight-600 w-80 md-w-90 sm-w-100 mx-auto mb-0 text-uppercase">Coming to visit?</h1>
+                        <!-- end page title -->
+                    </div>
+                    <div class="down-section text-center"><a href="#first-time" class="inner-link"><i class="ti-arrow-down icon-extra-small text-white-2 bg-blue padding-15px-all sm-padding-10px-all rounded-circle"></i></a></div>
+                </div>
+            </div>
+            <div class="swiper-container z-index-minus2 position-absolute top-0 h-100" data-slider-options='{ "loop": true, "effect":"fade", "slidesPerView": "1", "allowTouchMove":true, "autoplay": { "delay": 4000, "disableOnInteraction": false }, "keyboard": { "enabled": true, "onlyInViewport": true }, "navigation": { "nextEl": ".swiper-button-next", "prevEl": ".swiper-button-prev" }, "pagination": { "el": ".swiper-pagination-01", "clickable": true } }'>
+                <div class="swiper-wrapper">
+                    <!-- start slider item -->
+                    <div class="swiper-slide cover-background" style="background-image:url('images/teri-bagwell/teri-mothers-day.jpeg');"></div>
+                    <!-- end slider item -->
+                    <!-- start slider item -->
+                    <div class="swiper-slide cover-background" style="background-image:url('images/church/meet-and-greet2.jpg');"></div>
+                    <!-- end slider item -->
+                    <!-- start slider item -->
+                    <div class="swiper-slide cover-background" style="background-image:url('images/church/aaron-coaching-min.JPG');"></div>
+                    <!-- end slider item -->
+                    <!-- start slider item -->
+                    <div class="swiper-slide cover-background" style="background-image:url('images/church/meet-and-greet1.jpg');"></div>
+                    <!-- end slider item -->
+                    <!-- start slider item -->
+                    <div class="swiper-slide cover-background" style="background-image:url('images/church/church-service2.JPG');"></div>
+                    <!-- end slider item -->
+                    <!-- start slider item -->
+                    <div class="swiper-slide cover-background" style="background-image:url('images/church/meet-and-greet3.jpg');"></div>
+                    <!-- end slider item -->
+                    <!-- start slider item -->
+                    <div class="swiper-slide cover-background" style="background-image:url('images/church/family-dinner4.JPG');"></div>
+                    <!-- end slider item -->
+                </div>
+                <!-- <div class="swiper-pagination swiper-pagination-01 swiper-pagination-white"></div> -->
+            </div>
+        </section>
+        
+
+        <!-- 
+            *******************
+            start first time 
+            visitor section 
+            *******************
+        -->
+
+        <!-- start services section -->
+        <section class="overflow-hidden wow animate__fadeIn" id="first-time">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-8 margin-four-bottom text-center last-paragraph-no-margin">
+                        <h5 class="alt-font text-extra-dark-grpay font-weight-500">We know how hard it is to find the right church. Being new can be a challenge. But we think church should be fun and helpful. We encourage you to fill out the first time visitor form so we can welcome you and have your kids pre-registered for check-in prior to service. </h5>
+                        <!-- <p class="w-80 d-inline-block sm-w-100 text-extra-large">We know how hard it is to find the right church. Being new can be a challenge. But we think church should be fun and helpful. We encourage you to fill out the first time visitor form so we can welcome you and have your kids pre-registered for check-in prior to service.</p> -->
+                    </div>
+                </div>
+                
+                <?php include './includes/service-cards-container.html';?>
+
+            </div>
+        </section>
+        <!-- end services section -->
+
+
+        <!-- start parallax feature box -->
+        <section class="p-0 wow animate__fadeIn bg-light-gray" style="display: none;">
+            <div class="container-fluid">
+                <div class="row row-cols-1 row-cols-lg-2"> 
+                    <div class="col wow animate__fadeInLeft padding-four-all md-padding-eight-all md-padding-15px-lr sm-padding-50px-tb">
+                        <div class="row m-0">
+                            <div class="col-12 col-xl-10 margin-six-bottom lg-margin-six-bottom md-margin-30px-bottom sm-no-margin-bottom">
+                                <h4 class="alt-font text-extra-dark-gray font-weight-600 text-center text-lg-start md-w-70 mx-auto mx-lg-0 sm-w-90 xs-w-100 sm-margin-30px-bottom">We are delivering beautiful digital products for you</h4>
+                            </div>
+                        </div>
+                        <div class="row row-cols-1 row-cols-xl-2 row-cols-lg-1 row-cols-sm-2 m-0">
+                            <!-- start features box item -->
+                            <div class="col margin-six-bottom md-margin-50px-bottom sm-margin-40px-bottom last-paragraph-no-margin">
+                                <div class="feature-box-5 position-relative">
+                                    <i class="icon-desktop text-medium-gray icon-medium"></i>
+                                    <div class="feature-content">
+                                        <div class="text-extra-dark-gray margin-5px-bottom alt-font font-weight-600">Powerful Website Builder</div>
+                                        <p class="w-95 sm-w-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end features box item -->
+                            <!-- start features box item -->
+                            <div class="col margin-six-bottom md-margin-50px-bottom sm-margin-40px-bottom last-paragraph-no-margin" data-wow-delay="0.2s">
+                                <div class="feature-box-5 position-relative">
+                                    <i class="icon-book-open text-medium-gray icon-medium"></i>
+                                    <div class="feature-content">
+                                        <div class="text-extra-dark-gray margin-5px-bottom alt-font font-weight-600">Different Layout Type</div>
+                                        <p class="w-95 sm-w-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end features box item -->
+                            <!-- start features box item -->
+                            <div class="col lg-margin-six-bottom sm-margin-40px-bottom last-paragraph-no-margin" data-wow-delay="0.4s">
+                                <div class="feature-box-5 position-relative">
+                                    <i class="icon-wallet text-medium-gray icon-medium"></i>
+                                    <div class="feature-content">
+                                        <div class="text-extra-dark-gray margin-5px-bottom alt-font font-weight-600">True Responsiveness</div>
+                                        <p class="w-95 sm-w-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end features box item -->
+                            <!-- start features box item -->
+                            <div class="col last-paragraph-no-margin" data-wow-delay="0.6s">
+                                <div class="feature-box-5 position-relative ">
+                                    <i class="icon-camera text-medium-gray icon-medium"></i>
+                                    <div class="feature-content">
+                                        <div class="text-extra-dark-gray margin-5px-bottom alt-font font-weight-600">Elegant / Unique design</div>
+                                        <p class="w-95 sm-w-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end features box item -->
+                        </div>
+                    </div> 
+                    <div class="col cover-background md-h-400px wow animate__fadeInRight" style="background-image:url('https://via.placeholder.com/945x663');"></div>
+                </div>
+            </div>
+        </section>
+        <!-- end parallax feature box -->
+
+
+        <!-- 
+        *******************
+        start beliefs section 
+        *******************
+        -->
+
+        <!-- start section -->
+        <section class="p-0 wow animate__fadeIn text-center text-md-start" id="beliefs" style="display: none;">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+
+
+                <!-- Left Image -->
+                <div class="col-12 col-xl-6 col-lg-6 cover-background" style="background-image: url('./images/general/praying-men-arms-out.jpg')"><div class="md-h-500px sm-h-350px"></div></div>
+                    
+                    <!-- Right Text Background White -->
+                    <div class="col-12 col-xl-6 col-lg-6 bg-white last-paragraph-no-margin text-center text-lg-start">
+                        <div class="padding-80px-all xl-padding-40px-all md-padding-60px-all sm-padding-40px-all title-xxxl">
+                            <h5 class="alt-font font-weight-700 text-extra-dark-gray text-uppercase">06</h5>
+                            <h5 class="alt-font font-weight-700 text-extra-dark-gray text-uppercase">Sanctification</h5>
+                            <div class="separator-line-horrizontal-medium-thick bg-extra-dark-gray w-15 md-w-30 margin-25px-bottom md-margin-20px-bottom sm-w-50 mx-auto mx-lg-0 sm-margin-15px-bottom"></div>
+                            <p class="w-95 lg-w-100 text-xxxl font-weight-500">We believe that sanctification, holiness and the overcoming life is God’s design for the Church, which is the Bride of Christ.</p>
+                            <p class="w-95 lg-w-100 text-xxxl font-weight-500">Ephesians 5:25-27</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <?php include './includes/footer.html';?>
+        
+    </body>
+</html>
